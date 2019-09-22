469265fa06c406407e0807a850a8184fe9344b52
Carlos Hatus <carlos.hatus@hotmail.com>
2019-09-22 15:14:13 -0300

    android sdk

--- react-native-install.sh
+++ react-native-install.sh
@@ -6,6 +6,10 @@ sudo apt-get update && \
 sudo apt-get install -y \
     build-essential \
     curl \
+	gcc-multilib \
+	lib32z1 \
+	lib32stdc++6 \
+	unzip && \
 
 # Node.js
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && \
@@ -17,13 +21,25 @@ npm -v && \
 sudo npm install -g react-native-cli && \
 
 # Java 8 OpenJDK
-sudo apt-get install -y lib32stdc++6 lib32z1 openjdk-8-jdk && \
+sudo add-apt-repository ppa:openjdk-r/ppa -y && \
+sudo apt-get update && \
+sudo apt-get install -y openjdk-8-jdk && \
+java -version && \
 
-# Android Studio
-sudo add-apt-repository ppa:maarten-fonville/android-studio -y && \
-sudo apt update && \
-sudo apt install android-studio -y && \
+# Android SDK
+mkdir ~/Android -p && \
+cd ~ && \
+sudo curl -L https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip -o Android/Sdk.zip && \
+unzip Android/Sdk.zip -d Android/Sdk && \
+export ANDROID_HOME=~/Android/Sdk && \
+export PATH=$PATH:$ANDROID_HOME/tools && \
+export PATH=$PATH:$ANDROID_HOME/platform-tools && \
+# echo ANDROID_HOME=~/Android/Sdk >> ~/.profile && \
+# echo PATH=$PATH:$ANDROID_HOME/tools >> ~/.profile && \
+# echo PATH=$PATH:$ANDROID_HOME/platform-tools >> ~/.profile && \
+~/Android/Sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-27" "build-tools;27.0.3" && \
 android && \
 
 # limpar instalação
+rm -f ~/Android/Sdk.zip && \
 sudo apt autoremove -y
